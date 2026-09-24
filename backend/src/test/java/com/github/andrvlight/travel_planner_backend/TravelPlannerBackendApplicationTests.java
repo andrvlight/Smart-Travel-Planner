ffe735726b9e5d8375b437185b43099a6d0fefbe
Andrii <andrvlight@gmail.com>
2026-09-24 15:26:41 +0200

    Init project structure

---
+++ backend/src/test/java/com/github/andrvlight/travel_planner_backend/TravelPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andrvlight.travel_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
